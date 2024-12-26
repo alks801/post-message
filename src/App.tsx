52e811b4a222d7f1d458c072eb829d6e669390a2
Aleksandr Denisevich <denisevich.aa@gmail.com>
2024-12-26 11:23:46 +0300

    feat: more buttons

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,23 @@ function postParentMessage<T extends string>(type: T, payload?: Record<string, u
         type,
         payload,
       },
-      '*'
-    );
-    alert("Sent");
+      "*",
+    )
+    alert("Sent")
   } catch (err) {
-    alert("Can't post message to parent");
-    console.error("Can't post message to parent", err);
+    alert("Can't post message to parent")
+    console.error("Can't post message to parent", err)
+  }
+}
+
+function showAndroidToast(toast: string) {
+  try {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore
+    window.Android.showToast(toast)
+  } catch (err) {
+    alert("Can't show toast")
+    console.error("Can't post message to parent", err)
   }
 }
 
@@ -20,7 +31,9 @@ function App() {
   return (
     <>
       <h1>Test Post Message</h1>
-      <button onClick={() => postParentMessage('PW_WIDGET_OPEN_RULES_PAGE')}>Send</button>
+      <button onClick={() => postParentMessage("PW_WIDGET_OPEN_RULES_PAGE")}>Send via parent.postMessage</button>
+      <button onClick={() => showAndroidToast("PW_WIDGET_OPEN_RULES_PAGE")}>showAndroidToast</button>
+      <button onClick={() => alert("Test")}>Test Alert</button>
     </>
   )
 }
